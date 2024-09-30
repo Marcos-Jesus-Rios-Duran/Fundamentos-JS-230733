c0b0a53d7f74c8a1c8063acdf5a98daa8eb2ef63
Marcos Jesús Ríos Duran <230733@utxicotepec.edu.mx>
2024-09-30 08:55:19 -0600

    Actualización del archivo Ejercicio3.js
    Se añadió el punto 12 faltante

--- JS/Ejercicio3.js
+++ JS/Ejercicio3.js
@@ -247,7 +247,24 @@ console.log("%c2.- Objeto ", style_console);
         console.log(Venta2)
         console.table(Venta2)
 
-        console.log("%c12.- Mutabilidad POST Unión de objetos",style_console)
-        // vamos a verificar el status de mutabilidad de los objetos 
-        console.log("vamos a verificar el estatus de mutabilidad del objeto PEDIDO")
-        console.log(`Esta el objeto de Pedido Congelado ? : ${Object.isFrozen(Pedido)}`)
+        console.log("%c12.- Mutabilidad POST Unión de Objetos", style_console);
+
+
+
+// Vamos a verificar el estatus de mutabilidad de los obetos
+console.log("Vamos a verificar el estatus de mutabilidad del objeto PEDIDO")
+console.log(`Esta el objeto de Pedido Congelado ? : ${Object.isFrozen(Pedido)}` );
+console.log(`Esta el objeto de Pedido Sellado? : ${Object.isSealed(Pedido)}` );
+
+console.log("Vamos a verificar el estatus de mutabilidad del objeto COMPRADOR")
+console.log(`Esta el objeto de Pedido Congelado ? : ${Object.isFrozen(Comprador)}` );
+console.log(`Esta el objeto de Pedido Sellado? : ${Object.isSealed(Comprador)}` );
+
+console.log("Vamos a verificar el estatus de mutabilidad del objeto PRODUCTO")
+console.log(`Esta el objeto de Pedido Congelado ? : ${Object.isFrozen(Producto)}` );
+console.log(`Esta el objeto de Pedido Sellado? : ${Object.isSealed(Producto)}` );
+
+// Modificamos la estructura de producto, agregando una nueva propiedad
+Producto['isLegacy']=false;
+console.log(Producto)
+console.log(Venta2);
